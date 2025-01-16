1d465ef4c2b706a07dd7748ddd270f6a8373c0fc
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2025-01-16 04:25:51 -0500

    Users can register from the frontend. After registration, the user is logged in

--- src/register/Register.tsx
+++ src/register/Register.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import "./Register.css";
-import { Button, TextField } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Button, CircularProgress, TextField } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import httpModule from "../helpers/http.module";
+import { useJwt } from "../context/Jwt.context";
 
 const Register = () => {
   const [user, setUser] = useState({
@@ -10,56 +12,95 @@ const Register = () => {
     confirmPassword: "",
     username: "",
   });
+  const [loading, setLoading] = useState<boolean>(false);
+  const jwt = useJwt();
+  const navigate = useNavigate();
 
-  function handleSave() {}
+  function handleSave() {
+    if (
+      user.email === "" ||
+      user.password === "" ||
+      user.confirmPassword == "" ||
+      user.username == ""
+    ) {
+      alert("Fill  all fields");
+      return;
+    }
+
+    if (/\w+@[a-z]+.[a-z]+/.test(user.email) == false) {
+      alert("Please enter a valid email");
+      return;
+    }
+
+    if (user.password != user.confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    setLoading(true);
+    let email = user.email;
+    let password = user.password;
+    let username = user.username;
+    httpModule
+      .post("/auth/register", { email, password, username })
+      .then(async () => {
+        await jwt.login(user.email, user.password);
+        navigate("/");
+      })
+      .catch((error) => {
+        alert("Error");
+        console.log(error.response);
+        setLoading(false);
+      });
+  }
 
   return (
     <div className="content">
-      <div className="register">
-        <h2>Register</h2>
-        <TextField
-          autoComplete="off"
-          label="Email"
-          variant="outlined"
-          value={user.email}
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-        />
-        <TextField
-          autoComplete="off"
-          label="Username"
-          variant="outlined"
-          value={user.username}
-          onChange={(e) => setUser({ ...user, username: e.target.value })}
-        />
-        <TextField
-          autoComplete="off"
-          label="Password"
-          variant="outlined"
-          value={user.password}
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-          type="password"
-        />
-        <TextField
-          autoComplete="off"
-          label="Confirm Password"
-          variant="outlined"
-          value={user.confirmPassword}
-          onChange={(e) =>
-            setUser({ ...user, confirmPassword: e.target.value })
-          }
-          type="password"
-        />
-        <Button
-          variant="outlined"
-          color="primary"
-          onClick={handleSave}
-        >
-          Save
-        </Button>
-        <p>
-          Already a member? <Link to="/login">Login</Link>
-        </p>
-      </div>
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <div className="register">
+          <h2>Register</h2>
+          <TextField
+            autoComplete="off"
+            label="Email"
+            variant="outlined"
+            value={user.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
+          <TextField
+            autoComplete="off"
+            label="Username"
+            variant="outlined"
+            value={user.username}
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
+          />
+          <TextField
+            autoComplete="off"
+            label="Password"
+            variant="outlined"
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            type="password"
+          />
+          <TextField
+            autoComplete="off"
+            label="Confirm Password"
+            variant="outlined"
+            value={user.confirmPassword}
+            onChange={(e) =>
+              setUser({ ...user, confirmPassword: e.target.value })
+            }
+            type="password"
+          />
+          <Button variant="outlined" color="primary" onClick={handleSave}>
+            Save
+          </Button>
+          <p>
+            Already a member? <Link to="/login">Login</Link>
+          </p>
+        </div>
+      )}
     </div>
   );
 };
