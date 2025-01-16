55e2218c18fd5e72d12dc905fe90a9fd31a4dec7
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2025-01-16 11:11:59 -0500

    Finished function handleSendResetLink

--- src/password/ForgotPassword.tsx
+++ src/password/ForgotPassword.tsx
@@ -2,10 +2,29 @@ import { Button, TextField } from "@mui/material";
 import "./Password.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import httpModule from "../helpers/http.module";
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState("");
-  function handleSendResetLink() {}
+  
+  function handleSendResetLink() {
+    if (email == "") {
+      alert("Please enter your email");
+      return;
+    }
+
+    let params = new URLSearchParams();
+    params.append("email", email);
+    httpModule
+      .post("/auth/forgotPassword?" + params)
+      .then(() => {
+        alert("Check your email for the reset link");
+      })
+      .catch((error) => {
+        alert("Error");
+        console.log(error.response);
+      });
+  }
   return (
     <div className="content">
       <div className="forgotPassword">
